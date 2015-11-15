892ba510533fe08426802342902bc1b9dda65572
Kenneth Kousen <ken.kousen@kousenit.com>
2015-11-15 08:03:53 -0800

    Add assignment demo

--- src/main/groovy/nfjs/collections/PersonDemo.java
+++ src/main/groovy/nfjs/collections/PersonDemo.java
@@ -3,6 +3,7 @@ package nfjs.collections;
 import nfjs.collections.StringSamples;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class PersonDemo {
